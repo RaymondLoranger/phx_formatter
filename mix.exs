14401f7f78cfea59a5aa1b2f5b268c10077e3426
RaymondLoranger <Raymond.Loranger@bell.net>
2021-01-28 12:34:56 -0500

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,10 +4,33 @@ defmodule PhxFormatter.MixProject do
   def project do
     [
       app: :phx_formatter,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      name: "Phx Formatter",
+      source_url: source_url(),
+      description: description(),
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp source_url do
+    "https://github.com/RaymondLoranger/phx_formatter"
+  end
+
+  defp description do
+    """
+    Exports formatter configuration for a Phoenix project.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Raymond Loranger"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => source_url()}
     ]
   end
 
