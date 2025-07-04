f85cd507e6e0c3745e91dddaac5254def3e31360
RaymondLoranger <Raymond.Loranger@bell.net>
2025-07-04 08:04:53 -0400

    0.1.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhxFormatter.MixProject do
   def project do
     [
       app: :phx_formatter,
-      version: "0.1.8",
+      version: "0.1.9",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Phx Formatter",
